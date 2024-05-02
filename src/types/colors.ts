2fb3a54848510c5c0712f5bdb3e44f784361f6a4
Ruan-M2 <ruanazeredo@gmail.com>
2024-05-02 08:42:34 -0300

    :construction: add events theme

---
+++ src/types/colors.ts
@@ -0,0 +1,8 @@
+export const eventsTheme = {
+    "default" : "#fff",
+    "color1" : "#042940",
+    "color2" : "#005C53",
+    "color3" : "#9FC131",
+    "color4" : "#DBF227",
+    "color5" : "#D6D58E"
+}
